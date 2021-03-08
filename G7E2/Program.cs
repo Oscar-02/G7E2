a4abd84ea43b3587cc0392a71b918e67c7a94517
Oscar Cañas <oscacanas29@gmail.com>
2021-03-07 18:43:06 -0600

    1.0 Final Realise

---
+++ G7E2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G7E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i = 0;
+            try
+            {
+                Console.WriteLine("Ingrese el numero de * que quiere que imprima...");
+                num = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("El formato de numero no es el correcto...");
+                Console.WriteLine("El programa se cerrara...");
+                Console.ReadKey();
+                return;
+            }
+            Console.Clear();
+            do
+            {
+                Console.Write('*');
+                i++;
+            }
+            while (num != i);
+            Console.ReadKey();
+        }
+    }
+}
